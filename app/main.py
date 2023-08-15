bf3c27d5b968e25c6cc31bb1ad645b4b450c114a
Mostafa <m.bayat1376@gmail.com>
2023-08-15 14:35:15 +0330

    Edit some changes

--- app/main.py
+++ app/main.py
@@ -10,10 +10,10 @@ with open('score', 'r+') as score_file:
 def log(reason, old_score, dec_or_inc, score):
     log = dict()
     log["date"] = jdatetime.datetime.now().isoformat()
+    log["type"] = dec_or_inc
     log["score"] = score
     log["old_score"] = old_score
-    log["new_score"] = old_score + score
-    log["type"] = dec_or_inc
+    log["new_score"] = old_score + score if dec_or_inc == "increase" else old_score - score
     log["reason"] = reason
     with open('score.log', 'a+') as score_log:
         score_log.write(str(log) + '\n')
